bfdbb343d00d7bdaca42c64277daf7d116c7d03e
raja <rajakumarshah216@gmail.com>
2026-05-19 00:05:16 +0545

    test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()], base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
